6a7fd13b5794f2bedd35cea1a0efe8555bdd4726
Leif <leifar93@gmail.com>
2018-10-10 20:23:46 +0200

    Read accelerometer nr 2

--- Measure_angle_Arduino_MPU6050.ino/Measure_angle_Arduino_MPU6050.ino.ino
+++ Measure_angle_Arduino_MPU6050.ino/Measure_angle_Arduino_MPU6050.ino.ino
@@ -103,17 +103,17 @@ void loop(){
 
 void setup_mpu_6050_registers(){
   //Activate the MPU-6050
-  Wire.beginTransmission(0x68);                                        //Start communicating with the MPU-6050
+  Wire.beginTransmission(0x69);                                        //Start communicating with the MPU-6050
   Wire.write(0x6B);                                                    //Send the requested starting register
   Wire.write(0x00);                                                    //Set the requested starting register
   Wire.endTransmission();                                             
   //Configure the accelerometer (+/-8g)
-  Wire.beginTransmission(0x68);                                        //Start communicating with the MPU-6050
+  Wire.beginTransmission(0x69);                                        //Start communicating with the MPU-6050
   Wire.write(0x1C);                                                    //Send the requested starting register
   Wire.write(0x10);                                                    //Set the requested starting register
   Wire.endTransmission();                                             
   //Configure the gyro (500dps full scale)
-  Wire.beginTransmission(0x68);                                        //Start communicating with the MPU-6050
+  Wire.beginTransmission(0x69);                                        //Start communicating with the MPU-6050
   Wire.write(0x1B);                                                    //Send the requested starting register
   Wire.write(0x08);                                                    //Set the requested starting register
   Wire.endTransmission();                                             
@@ -121,10 +121,10 @@ void setup_mpu_6050_registers(){
 
 
 void read_mpu_6050_data(){                                             //Subroutine for reading the raw gyro and accelerometer data
-  Wire.beginTransmission(0x68);                                        //Start communicating with the MPU-6050
+  Wire.beginTransmission(0x69);                                        //Start communicating with the MPU-6050
   Wire.write(0x3B);                                                    //Send the requested starting register
   Wire.endTransmission();                                              //End the transmission
-  Wire.requestFrom(0x68,14);                                           //Request 14 bytes from the MPU-6050
+  Wire.requestFrom(0x69,14);                                           //Request 14 bytes from the MPU-6050
   while(Wire.available() < 14);                                        //Wait until all the bytes are received
   acc_x = Wire.read()<<8|Wire.read();                                  
   acc_y = Wire.read()<<8|Wire.read();                                  
