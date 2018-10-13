1008fb4a73194196848388c6e021b13d545c4ee0
John Chen <john20chen@gmail.com>
2018-10-13 11:44:30 +0200

    major refactoring

---
+++ arduino_reader.py
@@ -0,0 +1,25 @@
+import sys, serial, logging
+
+
+class ArduinoReader:
+	def __init__(self, arduino_port, baudrate):
+		logging.basicConfig(level=logging.DEBUG, filename='log.txt')
+		self.arduino_unit = serial.Serial(arduino_port, baudrate)
+
+	def _read_arduino(self):
+		while True:
+			try:
+				msg = self.arduino_unit.readline().decode('utf-8').split()
+				#print('roll: %s, pitch: %s, yaw: %s' % (msg[0], msg[1], msg[2]))
+				assert len(msg) == 3
+				yield list(map(float, msg))
+			except AssertionError as e:
+				sys.stderr.write(str(e))
+				logging.exception('ArduinoReader: Unclean reading from arduino')
+			except Exception as e:
+				sys.stderr.write(str(e))
+				logging.exception('ArduinoReader: Error when reading from serial')
+
+	def read(self):
+		return self._read_arduino
+
--- socket-xp.py
+++ socket-xp.py
@@ -1,81 +1,23 @@
-import socket, sys, struct, logging, serial
-from time import sleep
-
-
-def null_terminate(s):
-	return s + '\0'
-
-
-def create_null_pad(pad_length):
-	return ('\0'*pad_length).encode()
-
-
-def create_dref_packet(value, name):
-	header = null_terminate('DREF')
-	name = null_terminate(name)
-	pad_length = 509 - (len(header) + 4 + len(name))
-	pad = create_null_pad(pad_length)
-
-	packer = struct.Struct('<%ds f %ds %ds' % (len(header), len(name), pad_length))
-	return packer.pack(*(header.encode(), value, name.encode(), pad))
-
-
-def udp_server(udp_sock, address):
-	try:
-		print('Binding..')
-		udp_sock.bind(address)
-		print('UDP: Listening to %s %s' % address)
-		while True:
-			data, addr = udp_sock.recvfrom(1024)
-			print(data.hex().upper())
-	except socket.error as e:
-		sys.stderr.write('Socket Error: %s' % str(e))
-	except Exception as e:
-		sys.stderr.write('Error: %s' % str(e))
-	finally:
-		sock.close()
-		print('Socket closed')
-		sys.exit(0)
-
-
-names = {0: 'a', 1: 'b', 2: 'c'}
-def read_arduino():
-	try:
-		msg = ser.readline().decode('utf-8').split()
-		#msg = list(map(float, msg))
-		print('roll: %s, pitch: %s, yaw: %s' % (msg[0], msg[1], msg[2]))
-		#return msg
-		for i, data in enumerate(msg):
-			yield create_dref_packet(data, names[i])
-	except Exception as e:
-		sys.stderr.write(str(e))
-		logging.exception('Error when reading from serial')
-
-
-def udp_client(udp_sock, address, message_generator):
-	name = 'sim/test/test_float'
-	while True:
-		value = float(input('Set new value ~> '))
-		#message = create_dref_packet(value, name)
-		messages = message_generator()
-		for message in messages:
-			udp_sock.sendto(message, address)
-			print('send')
+import socket, sys
+from arduino_reader import ArduinoReader
+from networking import XPlaneDataAdapter, udp_client, udp_server
+from xp_vars import *
 
 
 def main():
-	#arg = sys.argv[1]
-	logging.basicConfig(level=logging.DEBUG, filename='log.txt')
-	ser = serial.Serial('/dev/ttyUSB0', 115200)
-	error_logs = open('log.txt', 'a')
+	xp_adapter = XPlaneDataAdapter()
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	mc_reader = ArduinoReader('/dev/ttyUSB2', 115200)
 
 	local_ip = '0.0.0.0'
-	destination_ip = '10.24.11.21'
+	destination_ip = '192.168.1.136'
 	port = 49000
 
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	udp_client(sock, (destination_ip, port), read_arduino)
-	#udp_client(sock, (destination_ip, port))
+	udp_client(
+			sock,
+			(destination_ip, port),
+			xp_adapter.wrap(mc_reader.read(), expected_reading)
+			)
 	#udp_server(sock, (local_ip, port))
 
 
