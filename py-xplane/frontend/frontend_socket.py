3977d08859fded644247ad0e570892263d4330b2
John Chen <john20chen@gmail.com>
2018-10-29 10:56:15 +0100

    rewrite frontend_socket for tcp socket instead of async websocket

--- py-xplane/frontend/frontend_socket.py
+++ py-xplane/frontend/frontend_socket.py
@@ -1,31 +1,40 @@
-import asyncio, websockets, random
+import random, sys, socket
+from time import sleep
+from _thread import start_new_thread
 
 
-async def recvmsg(websocket, path):
-	print('ok')
-	try:
-		async for message in websocket:
-			print(message)
-		print('lost connection')
-	except Exception as e:
-		print('Error in receivemsg: %s' % str(e))
+# TODO: write generic socket tools
+class FrontendSocket:
+	def __init__(self, address, handler):
+		self.address = address
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.handler = handler
 
+	def _socket_receive(self, sock):
+		while True:
+			data = sock.recv(1024)
+			if not data: raise ConnectionError('Broken pipe, no more data received')
+			self.handler(data.decode('utf-8').strip())
 
-async def sendmsg(websocket, path):
-	while True:
-		await asyncio.sleep(1)
-		await websocket.send(str(random.random()))
+	def connect_frontend(self):
+		try:
+			self.sock.bind(self.address)
+			self.sock.listen(1)
+			print('TCP server listening to frontend at %s %s' % self.address)
+			conn, addr = self.sock.accept()
+			start_new_thread(self._socket_receive, (conn,))
+			while True:
+				conn.send(str(random.random()).encode('utf-8'))
+				sleep(0.2)
+		except socket.error as e:
+			sys.stderr.write('Connection failed: %s' % str(e))
+		except Exception as e:
+			sys.stderr.write('Exception: %s' % str(e))
+		finally:
+			self.sock.close()
+			print('Socket closed')
 
+	def run(self):
+		start_new_thread(self.connect_frontend, ())
 
-async def handler(websocket, path):
-	msg_in = asyncio.ensure_future(recvmsg(websocket, path))
-	msg_out = asyncio.ensure_future(sendmsg(websocket, path))
-	done, pending = await asyncio.wait([msg_in, msg_out], return_when=asyncio.FIRST_COMPLETED)
-	for task in pending:
-		task.cancel()
-
-
-start_server = websockets.serve(handler, '0.0.0.0', 8001)
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
 
