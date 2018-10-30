7bb0c411ea996df3eea249778df3601aa3f48425
John Chen <john20chen@gmail.com>
2018-10-30 08:08:07 +0100

    check for multiple provided arduino ports and use lazy loading

--- py-xplane/main.py
+++ py-xplane/main.py
@@ -7,14 +7,16 @@ from _thread import start_new_thread
 
 
 def main():
+	arduino_ports = ['/dev/ttyUSB1', '/dev/ttyUSB2']
+
 	xp_adapter = XPlaneConnector()
 	xplane_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	frontend_sock = FrontendSocket(('localhost', 8005), xp_adapter.frontend_handler).run()
-	data_reader = ArduinoReader('/dev/ttyUSB1', 115200)
 
 	local_ip = '0.0.0.0'
 	destination_ip = '192.168.1.136'
 	port = 49000
+	data_reader = ArduinoReader(arduino_ports, 115200)
 
 	udp_client(
 			xplane_sock,
--- py-xplane/xplane/arduino_reader.py
+++ py-xplane/xplane/arduino_reader.py
@@ -2,24 +2,31 @@ import sys, serial, logging
 
 
 class ArduinoReader:
-	def __init__(self, arduino_port, baudrate):
+	def __init__(self, arduino_ports, baudrate):
 		logging.basicConfig(level=logging.DEBUG, filename='log.txt')
-		self.arduino_unit = serial.Serial(arduino_port, baudrate)
+		self.baudrate = baudrate
+		self.arduino_ports = arduino_ports
 
 	def _read_arduino(self):
-		while True:
+		for port in self.arduino_ports:
 			try:
-				msg = self.arduino_unit.readline().decode('utf-8').split()
-				#print('roll: %s, pitch: %s, yaw: %s' % (msg[0], msg[1], msg[2]))
-				assert len(msg) == 4
-				yield list(map(float, msg))
-			except AssertionError:
-				error_msg = 'ArduinoReader: Unclean reading from arduino, %d' % len(msg)
-				sys.stderr.write(error_msg)
-				logging.exception(error_msg)
-			except Exception as e:
-				sys.stderr.write('error: %s\n' % str(e))
-				logging.exception('ArduinoReader: Error when reading from serial')
+				print('Trying %s..' % port)
+				self.arduino_unit = serial.Serial(port, self.baudrate)
+				while True:
+					try:
+						msg = self.arduino_unit.readline().decode('utf-8').split()
+						assert len(msg) == 4
+						yield list(map(float, msg))
+					except AssertionError:
+						error_msg = 'ArduinoReader: Unclean reading from arduino, %d\n' % len(msg)
+						sys.stderr.write(error_msg)
+						logging.exception(error_msg)
+					except Exception as e:
+						sys.stderr.write('error: %s\n' % str(e))
+						logging.exception('ArduinoReader: Error when reading from serial')
+			except serial.serialutil.SerialException as e:
+				sys.stderr.write('ArduinoReader error: %s\n' % str(e))
+				logging.exception('ArduinoReader: Did not find port %s\n' % port)
 
 	def read(self):
 		return self._read_arduino
