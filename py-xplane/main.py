c1904e049974031f64a988ddc3898b2a24764a5f
John Chen <john20chen@gmail.com>
2018-10-30 08:08:55 +0100

    reorganize main headers

--- py-xplane/main.py
+++ py-xplane/main.py
@@ -7,23 +7,22 @@ from _thread import start_new_thread
 
 
 def main():
+	local_ip = '0.0.0.0'
+	xplane_ip = '10.53.25.98'
+	xplane_port = 49000
 	arduino_ports = ['/dev/ttyUSB1', '/dev/ttyUSB2']
 
 	xp_adapter = XPlaneConnector()
 	xplane_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	frontend_sock = FrontendSocket(('localhost', 8005), xp_adapter.frontend_handler).run()
-
-	local_ip = '0.0.0.0'
-	destination_ip = '192.168.1.136'
-	port = 49000
 	data_reader = ArduinoReader(arduino_ports, 115200)
 
 	udp_client(
 			xplane_sock,
-			(destination_ip, port),
+			(xplane_ip, xplane_port),
 			xp_adapter.wrap(data_reader.read(), expected_reading)
 			)
-	#udp_server(sock, (local_ip, port))
+	#udp_server(sock, (local_ip, xplane_port))
 
 
 if __name__ == "__main__":
