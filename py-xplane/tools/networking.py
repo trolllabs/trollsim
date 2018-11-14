b07b00175f52762df1d798222ce329f9b9888754
John Chen <john20chen@gmail.com>
2018-11-14 18:51:14 +0100

    add somewhat more generic tcp server

--- py-xplane/tools/networking.py
+++ py-xplane/tools/networking.py
@@ -32,3 +32,30 @@ class UDPClient:
 			data = self.sock.recv(1024)
 			yield data
 
+
+class TCPServer:
+	'''
+	Accepts only one connection
+	'''
+	def __init__(self, host, port):
+		self.address = (host, port)
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		print('TCP server listening to frontend at %s %s' % self.address)
+		self.sock.bind(self.address)
+		self.sock.listen(1)
+		self.conn, self.addr = self.sock.accept()
+
+	def send(self, message):
+		self.conn.send(message.encode('utf-8'))
+
+	def read(self):
+		while True:
+			data = self.conn.recv(1024)
+			if not data: raise ConnectionError('Broken pipe, no more data received')
+			yield data
+
+	def close(self):
+		self.sock.close()
+		print('TCP Server closed.')
+
