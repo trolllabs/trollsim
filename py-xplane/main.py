160391a34182dd565af61b8607ceacf1c3a22b41
John Chen <john20chen@gmail.com>
2018-11-14 18:55:59 +0100

    major refactor, readers now support multiple listeners

--- py-xplane/main.py
+++ py-xplane/main.py
@@ -1,26 +1,42 @@
-import socket, sys, logging
-from frontend.frontend_socket import FrontendSocket
-from readers import ArduinoReader
-from tools.networking import udp_server, UDPClient
-from tools.xplane_variables import *
-from data_processors.xplane_processor import XPlaneConnector
-from _thread import start_new_thread
+import sys, logging, threading
+from readers import ArduinoReader, SocketReader
+from tools.networking import udp_server, UDPClient, TCPServer
+from processors import GloveMultiplier, FrontendSocket
 
 
 def main():
-	local_ip = '0.0.0.0'
 	logging.basicConfig(level=logging.DEBUG, filename='log.txt')
 	xplane_ip = '10.53.25.98'
 	xplane_port = 49000
-	arduino_ports = ['/dev/ttyUSB1', '/dev/ttyUSB2']
+	frontend_ip = 'localhost'
+	frontend_port = 8005
+	glove_ports = ['/dev/ttyUSB1', '/dev/ttyUSB2']
+	ehealth_ports = ['/dev/ttyACM0']
 
-	xp_adapter = XPlaneConnector(expected_reading)
-	xplane_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	frontend_sock = FrontendSocket(('localhost', 8005), xp_adapter.frontend_handler).run()
-	data_reader = ArduinoReader(arduino_ports, 115200)
+	xplane_socket = UDPClient(xplane_ip, xplane_port)
+	frontend_socket = TCPServer(frontend_ip, frontend_port)
 
-	xp_adapter(data_reader.read(), UDPClient(xplane_ip, xplane_port).send)
-	#udp_server(sock, (local_ip, xplane_port))
+	ehealth_reader = ArduinoReader(ehealth_ports, 115200)
+	glove_reader = ArduinoReader(glove_ports, 115200)
+	frontend_reader = SocketReader(frontend_socket.read)
+	xplane_reader = SocketReader(xplane_socket.read)
+
+	glove_processor = GloveMultiplier(xplane_socket.send, glove_reader, frontend_reader)
+	frontend = FrontendSocket(
+			glove_processor.frontend_handler,
+			frontend_socket.send,
+			frontend_reader,
+			glove_reader)
+
+	frontend_reader()
+	xplane_reader()
+	ehealth_reader()
+	glove_reader()
+
+
+	# Very temporary solution
+	from time import sleep
+	sleep(2**63/10**9-1)
 
 
 if __name__ == "__main__":
