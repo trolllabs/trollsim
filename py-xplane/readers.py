e7ad7c82f7f214595c8268e1d7cb00f849282d0c
John Chen <john20chen@gmail.com>
2018-11-14 18:45:45 +0100

    move data assertion outside reader

--- py-xplane/readers.py
+++ py-xplane/readers.py
@@ -13,16 +13,11 @@ class ArduinoReader:
 				self.arduino_unit = serial.Serial(port, self.baudrate)
 				while True:
 					try:
-						msg = self.arduino_unit.readline().decode('utf-8').split()
-						assert len(msg) == 4
-						yield list(map(float, msg))
-					except AssertionError:
-						error_msg = 'ArduinoReader: Unclean reading from arduino, %d\n' % len(msg)
-						sys.stderr.write(error_msg)
-						logging.exception(error_msg)
-					except Exception as e:
-						sys.stderr.write('error: %s\n' % str(e))
-						logging.exception('ArduinoReader: Error when reading from serial')
+						reading = self.arduino_unit.readline().decode('utf-8')
+						if reading:
+							yield reading
+					except UnicodeDecodeError as e:
+						sys.stderr.write('Decode error at ArduinoReader: %s\n' % str(e))
 			except serial.serialutil.SerialException as e:
 				sys.stderr.write('ArduinoReader error: %s\n' % str(e))
 				logging.exception('ArduinoReader: Did not find port %s\n' % port)
