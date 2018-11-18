e1845b56cf7cacae75cd4a6cfca3bf69585ccb3c
Leif_laptop <leifar93@gmail.com>
2018-11-18 19:02:06 +0100

    Represent changes on 2 axis' with vibrations

--- vibrator/vibrator.ino
+++ vibrator/vibrator.ino
@@ -4,12 +4,15 @@ int motor3=9;
 int motor4=11;
 
 int angle1=0;
+int angle2=0;
 int axisVal1=0;
+int axisVal2=0;
+int angleInput=0;
 
 void setup() {
   // put your setup code here, to run once:
 
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   pinMode(motor1, OUTPUT);
   pinMode(motor2, OUTPUT);
@@ -18,24 +21,50 @@ void setup() {
 
 }
 
-
+int readInteger() {
+  byte int_buf[4];
+  Serial.readBytes(int_buf, 4);
+  int res = int_buf[0] << 24 | int_buf[1] << 16 | int_buf[2] << 8 | int_buf[3];
+  return res;
+}
 
 
 void loop() {
   // put your main code here, to run repeatedly:
 
   if (Serial.available() > 0) {
-    angle1= Serial.read();
+    angleInput= readInteger();
+    Serial.print("angleInput: ");
+    Serial.print(angleInput);
+    if(angleInput>=0 && angleInput<181){
+      angle1=abs(angleInput-90);
+      Serial.print(", angle 1:");
+      Serial.print(angle1); 
+      Serial.print(", angle 2: ");
+      Serial.println(angle2);
+    }
+    else if(angleInput>180 && angleInput<361){
+      angle2=abs(angleInput-270);
+      Serial.print(", angle 1:");
+      Serial.print(angle1); 
+      Serial.print(", angle 2:");
+      Serial.println(angle2); 
+    }
+    else{
+      angle1=0;
+      angle2=0;
+    }
   }
 
-  axisVal1=map(abs(angle1),0, 180, 0, 255);
+  axisVal1=map(angle1,0, 90, 0, 255);
+  axisVal2=map(angle2,0, 90, 0, 255);  
 
-    analogWrite(motor1, axisVal1);
+    analogWrite(motor1, axisVal2);
     analogWrite(motor2, axisVal1);
-    analogWrite(motor3, axisVal1);
+    analogWrite(motor3, axisVal2);
     analogWrite(motor4, axisVal1);
 
-  delay(1000);
+  //delay(10);
 
 
 }
