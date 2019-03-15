ddfa37ca0e7ba83fae3d157a0c3fd792acd369d0
johnche <john20chen@gmail.com>
2019-03-15 17:07:56 +0100

    update serial reader

--- snippets/arduino/serial_reader.py
+++ snippets/arduino/serial_reader.py
@@ -2,7 +2,7 @@ import serial, struct
 from time import sleep
 
 
-s = serial.Serial('/dev/ttyACM4', 115200)#, parity=serial.PARITY_EVEN)
+s = serial.Serial('/dev/ttyUSB0', 115200)#, parity=serial.PARITY_EVEN)
 
 
 '''
@@ -10,11 +10,13 @@ Expects:
 	byte 1 - sensor id
 	byte 2-5 - sensor data
 	byte 6 - newline character
+	big endianness
 '''
 while True:
 	reading = s.readline()
 
 	if (len(reading) == 6):
+		print(reading[:-1])
 		sensor_data = reading[1:-1]
 		sensor_id = reading[0]
 		print(sensor_id, sensor_data.hex().upper())
@@ -22,8 +24,8 @@ while True:
 		# Test float data
 		# 0x9EEF8340 test float: 4.123
 		# 0x05D4F642 test2 float: 123.4141
-		print('struct unpack', struct.unpack('f', sensor_data)[0])
+		print('struct unpack float:', struct.unpack('>f', sensor_data)[0])
 
 		# Test int32 data
-		print('struct unpack', struct.unpack('i', sensor_data)[0])
+		print('struct unpack int:', struct.unpack('>i', sensor_data)[0])
 
