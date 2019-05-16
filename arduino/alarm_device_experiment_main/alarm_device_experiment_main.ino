b24a31180ce24ea6bcad774bca83f0a8a827e59e
Leif Arne Hatlem <leifaha@ntnu.no>
2019-05-16 12:47:33 +0200

    Removed all delays in slave. Implemented audio volume control. Lowered amount of packages repeated without state changes.

--- arduino/alarm_device_experiment_main/alarm_device_experiment_main.ino
+++ arduino/alarm_device_experiment_main/alarm_device_experiment_main.ino
@@ -1,10 +1,8 @@
 const int packet_size = 6;
-
 typedef union {
   float floatingPoint;
   byte binary[4];
 } binaryFloat;
-
 void write_float(char id, float value) {
   binaryFloat reading;
   reading.floatingPoint = value;
@@ -19,7 +17,6 @@ void write_float(char id, float value) {
 
   Serial.write(res, packet_size);
 }
-
 void write_long(char id, long value32) {
   byte res[packet_size];
   res[0] = id;
@@ -31,191 +28,86 @@ void write_long(char id, long value32) {
 
   Serial.write(res, packet_size);
 }
-
 void write_int(char id, int value16) {
   long value32 = (long) value16;
   write_long(id, value32);
 }
-
 #include <Wire.h>
 
-int bPin2 = 3; //pin number 2nd button
-bool bVal2 = LOW; //value read from button 2
-int activePin = 7; //digital pin triggering device on/off.
-int alarmTypePin = 8; //digital pin triggering which alarm system is active
-int knobValue = 0;
-int scenarioValue = 0;
-
-bool active = 0; //0 or 1, decides if it will trigger alarms or not
-
-int alarms = 1; //value to hold number of alarms that will be triggered in total before device becomes inactive again
-int currentAlarm = 0; //counter counting how many alarms have happened
-unsigned long  minPeriod = 5000; // input in seconds here minimum time period between alarms
-unsigned long  maxPeriod = 7000; // input in seconds here maximum time period between alarms
-
-byte rVal = 0;
+/*******************************************************************
+  Variables for the experimenter to tweak alarm periods and haptic amplitude.
+********************************************************************/
+unsigned long  minPeriod = 5000; // input in milliseconds here minimum time period between alarms
+unsigned long  maxPeriod = 7000; // input in milliseconds here maximum time period between alarms
+unsigned long alarmPeriod = 10000; //var in milliseconds that decides time alarm sounds before it fails and stops. Max 16 bits (65535 because of wire.transmit constraints)
 
 int scen1Alarms = 2; //value deciding number of alarms in scenario 1
 int scen2Alarms = 3; //value deciding number of alarms in scenario 2
 int scen3Alarms = 5; //value deciding number of alarms in scenario 3
+//*******************************************************************
 
+/*******************************************************************
+  PIN DEFINITIONS
+ ********************************************************************/
+int bPin = 3; //pin number for button
+int activePin = 7; //digital pin triggering device on/off.
+int alarmTypePin = 8; //digital pin triggering which alarm system is active
+//******************************************************************
+
+/*******************************************************************
+  RANDOM VARIABLES
+ ********************************************************************/
+byte rVal = 0; //variable that is randomized to decide which alarm is triggered
+bool bVal = LOW; //value read from button 2
+int knobValue = 0; //variable that holds which scenario (1-3) is active
+int scenarioValue = 0;
+bool active = 0; //0 or 1, decides if it will trigger alarms or not
+
+int alarms = 1; //value to hold number of alarms that will be triggered in total before device becomes inactive again
+int currentAlarm = 0; //counter counting how many alarms have happened
 unsigned long currentTime = 0; //var to store current time using millis(), to be updated throughout
 unsigned long startTime = 0; //time when alarm starts
 unsigned long responseTime = 0; //response time to react to alarm
-unsigned long alarmPeriod = 10000; //var in milliseconds that decides how long the alarm should sound before alarm task is failed due to no action.
 unsigned long randomPeriod = 0; //Variable to hold ranomized waiting time untill alarm is triggered
 unsigned long limitTime = 0; //Variable to hold time limit where alarm task is failed due to no action
 
-int alarmType = 0; // Set to 0 for system A (light, sound) and 1 for system B (light, sound, haptic)
+int alarmType = 0; //Var to hold which alarm system is active (0 for system A (light, sound, 1 for system B (light, sound, haptic)
 int alarmSystemMod = 0; //Variable adjusting rVal in line with what system is active
+int disarmedMessageState=LOW;
 
-int scenarioKnob() {
-  int sensorValue = analogRead(A3);
-  if (sensorValue >= 0 && sensorValue < 100) {
-    knobValue = 1;
-  }
-  if (sensorValue >= 100 && sensorValue < 923) {
-    knobValue = 2;
-  }
-  if (sensorValue >= 924) {
-    knobValue = 3;
-  }
-  //Serial.println(knobValue);
-  return knobValue;
-}
-
-void setup() {
-  // put your setup code here, to run once:
-
-  pinMode(activePin, INPUT);
-  pinMode(bPin2, INPUT);
-  Serial.begin(115200);
-
-  Wire.begin(); //join i2c bus
-  randomSeed(analogRead(0));
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  //  Writing float:
-  //  write_float(0xAA, 123.4141);
-
-  //  Writing integer:
-  //  write_int(0xAA, 5234);
-
-
-  active = digitalRead(activePin); //Read value of on-switch
+//*******************************************************************
+void alarmTypeCheck() {
   alarmType = digitalRead(alarmTypePin); //Real value of alarmTypePin, deciding which system should be used.
-
-  if (active == HIGH && currentAlarm < alarms) {
-    write_int(12, 1); //Sends alarm=armed
-  }
   if (alarmType == 0) {
     alarmSystemMod = 0;
-    Serial.println("System A (light, sound) is active.");
+    write_int(12, 0); //Sends alarm type A
   }
   if (alarmType == 1) {
     alarmSystemMod = 3;
-    Serial.println("System B (light, sound, haptic) is active.");
+    write_int(12, 1); //Sends alarm type B
   }
-  scenarioValue = scenarioKnob();
-  write_int(17, scenarioValue); //Sends if scenario 1, 2 or 3 is active
-
-  if (scenarioValue == 1) {
+}
+void scenarioCheck() {
+  int sensorValue = analogRead(A3);
+  if (sensorValue >= 0 && sensorValue < 100) {
     alarms = scen1Alarms;
-    //Serial.println("Scenario 1 active");
+    write_int(13, 1); //Sends packet informing that scenario 1 is active
   }
-  if (scenarioValue == 2) {
+  if (sensorValue >= 100 && sensorValue < 923) {
     alarms = scen2Alarms;
-    //Serial.println("Scenario 2 active");
+    write_int(13, 2); //Sends packet informing that scenario 2 is active
   }
-  if (scenarioValue == 3) {
+  if (sensorValue >= 924) {
     alarms = scen3Alarms;
-    //Serial.println("Scenario 3 active");
+    write_int(13, 3); //Sends packet informing that scenario 3 is active
   }
+}
 
-  while (active == HIGH && currentAlarm < alarms) {      //Condition to execute when device is on/armed.
-    ++currentAlarm; //Increments alarm counter by 1
-    write_int(13, currentAlarm); //Sends alarm number
+void writeToSlave(byte alarmNumber) {
+  Wire.beginTransmission(1); ///transmit   to device #1
+  Wire.write(alarmNumber);
+  Wire.endTransmission();
 
-    currentTime = millis(); //reading of current time
-    randomPeriod = random(minPeriod, maxPeriod); //randomized waiting time untill first alarm is triggered
-    limitTime = currentTime + randomPeriod + alarmPeriod; //time where alarm task is failed due to no action
-    delay(randomPeriod);
-    rVal = random(1, 4);
-    //Serial.println(rVal);
-    switch (rVal) {
-      case 1: //Alarm scenario 1
-        write_int(14, rVal); //Sends ID of the triggeder alarm
-        startTime = millis();
-
-        writeToSlave(rVal + alarmSystemMod);
-
-        while (currentTime < limitTime) {
-          bVal2 = buttonCheck(bVal2, bPin2);
-          if (bVal2 == HIGH) {
-            write_int(15, 1); //Sends result of alarm test being passed
-            break;
-          }
-          currentTime = millis();
-          if (currentTime >= limitTime) {
-            write_int(15, 2); //Sends result of alarm test being failed due to inactivity
-          }
-        }
-        break;
-      case 2: //Alarm scenario 2
-        write_int(14, rVal); //Sends ID of the triggeder alarm
-        startTime = millis();
-
-        writeToSlave(rVal + alarmSystemMod);
-
-        while (currentTime < limitTime) {
-          bVal2 = buttonCheck(bVal2, bPin2);
-          if (bVal2 == HIGH) {
-            write_int(15, 1); //Sends result of alarm test being passed
-            break;
-          }
-          currentTime = millis();
-          if (currentTime >= limitTime) {
-            write_int(15, 2); //Sends result of alarm test being failed due to inactivity
-          }
-        }
-        break;
-      case 3: //Alarm scenario 3
-        write_int(14, rVal); //Sends ID of the triggeder alarm
-        //Serial.println("Alarm 3 triggered");
-        startTime = millis();
-
-        writeToSlave(rVal + alarmSystemMod);
-
-        while (currentTime < limitTime) {
-          bVal2 = buttonCheck(bVal2, bPin2);
-          if (bVal2 == HIGH) {
-            write_int(15, 1); //Sends result of alarm test being passed
-            break;
-          }
-          currentTime = millis();
-          if (currentTime >= limitTime) {
-            write_int(15, 2); //Sends result of alarm test being failed due to inactivity
-          }
-        }
-        break;
-      default:
-        write_int(15, rVal); //Sends that alarm number was invalid
-        //Serial.println("error in alarm cases");
-        break;
-    }
-    active = digitalRead(activePin); //Checks if device is still active and should continue looping
-    writeToSlave(0);
-  }
-
-  //Serial.println("Device disarmed");
-  write_int(12, 0); //Sends alarm=disarmed
-  delay(3000);
-  if (active == LOW) {
-    currentAlarm = 0;
-  }
 }
 
 bool buttonCheck(bool bValX, int buttonPinX) {
@@ -230,9 +122,65 @@ bool buttonCheck(bool bValX, int buttonPinX) {
   return newBValX;
 }
 
+void setup() {
+  // put your setup code here, to run once:
 
-void writeToSlave(byte alarmNumber) {
-  Wire.beginTransmission(1); ///transmit   to device #1
-  Wire.write(alarmNumber);
-  Wire.endTransmission();
+  pinMode(activePin, INPUT);
+  pinMode(bPin, INPUT);
+  Serial.begin(115200);
+
+  Wire.begin(); //join i2c bus
+  randomSeed(analogRead(0));
+
+}
+
+void loop() {
+  //  Writing float:
+  //  write_float(0xAA, 123.4141);
+  //  Writing integer:
+  //  write_int(0xAA, 5234);
+
+  active = digitalRead(activePin); //Read value of on-switch
+
+  if (active == HIGH && currentAlarm < alarms) {
+    write_int(11, 1); //Sends alarm=armed
+    disarmedMessageState=LOW; //Resets state variable allowing device to report when disarmed
+    alarmTypeCheck(); //Checks which alarm system is selected, sets alarmSystemMod-variable and sends packet informing of system A or B
+    scenarioCheck(); //Checks position of scenario switch, sets correct nuber of alarms and sends packet informing of scenario 1, 2 or 3
+  }
+  while (active == HIGH && currentAlarm < alarms) {      //Condition to execute when device is on/armed.
+    ++currentAlarm; //Increments alarm counter by 1
+    currentTime = millis(); //reading of current time
+    randomPeriod = random(minPeriod, maxPeriod); //randomized waiting time untill first alarm is triggered
+    limitTime = currentTime + randomPeriod + alarmPeriod; //time where alarm task is failed due to no action
+    delay(randomPeriod);
+    rVal = random(1, 4);
+    write_int(15, rVal); //Sends ID of the triggeder alarm
+    //write_int(14, currentAlarm); //Sends alarm number
+    startTime = millis();
+    writeToSlave(rVal + alarmSystemMod);
+    while (currentTime < limitTime) {
+      bVal = buttonCheck(bVal, bPin);
+      if (bVal == HIGH) {
+        write_int(16, 1); //Send packet reporting button was pushed.
+        writeToSlave(0); //Send signal to slave stopping alarms.
+        break;
+      }
+      currentTime = millis();
+      if (currentTime >= limitTime) {
+        write_int(15, 2); //Sends result of alarm test being failed due to inactivity
+        writeToSlave(0);
+      }
+    }
+    active = digitalRead(activePin); //Checks if device is still active and should continue looping
+  }
+  if(disarmedMessageState==LOW){
+    write_int(11, 0); //Sends packet informing that alarm device is disarmed.
+    disarmedMessageState=HIGH;
+  }
+  delay(1000);
+  if (active == LOW) {
+    currentAlarm = 0;
+  }
+
 }
--- arduino/alarm_device_experiment_slave/alarm_device_experiment_slave.ino
+++ arduino/alarm_device_experiment_slave/alarm_device_experiment_slave.ino
@@ -1,12 +1,8 @@
-
-
 const int packet_size = 6;
-
 typedef union {
   float floatingPoint;
   byte binary[4];
 } binaryFloat;
-
 void write_float(char id, float value) {
   binaryFloat reading;
   reading.floatingPoint = value;
@@ -21,7 +17,6 @@ void write_float(char id, float value) {
 
   Serial.write(res, packet_size);
 }
-
 void write_long(char id, long value32) {
   byte res[packet_size];
   res[0] = id;
@@ -33,50 +28,59 @@ void write_long(char id, long value32) {
 
   Serial.write(res, packet_size);
 }
-
 void write_int(char id, int value16) {
   long value32 = (long) value16;
   write_long(id, value32);
 }
 
-#include <Wire.h>
-
-int alarmNumber = 0; //variable to hold alarm number
-
-int ledPin1 = 3;  //pin number 1st led (PWM)
-int ledPin2 = 5;  //pin number 2nd led (PWM)
-int ledPin3 = 6;  //pin number 3rd led (PWM)
-
-#define enA 10 //enable pin for haptic feedback
-#define in1 11 //in1 pin for haptic feedback
-#define in2 12 //in2 pin for haptic feedback
-
-
-int voltageValue = 0; //variable to hold haptic motor voltage value
-int pwmOutput = 0;  //variable to hold pwmOutput value to haptic motor
 
 /*******************************************************************
   Variables for the experimenter to tweak alarm periods and haptic amplitude.
 ********************************************************************/
 
 // Alarm 1 settings:
-int A1OnPeriod = 100;   //Period in [ms] alarm 1 is activated every loop
-int A1OffPeriod = 200; // Period in [ms] alarm 1 is off every loop
-int A1Amplitude = 5;  //Amplitude in [V] of alarm 1's haptic feedback. Input valid from 0-9V.
+unsigned long A1OnPeriod = 1000;   //Period in [ms] alarm 1 is activated every loop
+unsigned long A1OffPeriod = 2000; // Period in [ms] alarm 1 is off every loop
+float A1Volume=-2; //Volume of audio alarm 1, in [dB]. Scale from -63,5dB (min) to 0dB (max).
+int A1HapticAmplitude = 9;  //Amplitude in [V] of alarm 1's haptic feedback. Input valid from 0-9V.
 
 // Alarm 2 settings:
-int A2OnPeriod = 200;   //Period in [ms] alarm 2 is activated every loop
-int A2OffPeriod = 100;  // Period in [ms] alarm 2 is off every loop
-int A2Amplitude = 7;  //Amplitude in [V] of alarm 1's haptic feedback. Input valid from 0-9V.
+unsigned long A2OnPeriod = 2000;   //Period in [ms] alarm 2 is activated every loop
+unsigned long A2OffPeriod = 1000;  // Period in [ms] alarm 2 is off every loop
+float A2Volume=-5.0; //Volume of audio alarm 2, in [dB]. Scale from -63,5dB (min) to 0dB (max).
+int A2HapticAmplitude = 7;  //Amplitude in [V] of alarm 1's haptic feedback. Input valid from 0-9V.
 
 // Alarm 3 settings:
-int A3OnPeriod =  150;  //Period in [ms] alarm 3 is activated every loop
-int A3OffPeriod = 150;  // Period in [ms] alarm 3 is off every loop
-int A3Amplitude = 9;  //Amplitude in [V] of alarm 1's haptic feedback. Input valid from 0-9V.
+unsigned long A3OnPeriod =  1500;  //Period in [ms] alarm 3 is activated every loop
+unsigned long A3OffPeriod = 1500;  // Period in [ms] alarm 3 is off every loop
+float A3Volume=-7; //Volume of audio alarm 3, in [dB]. Scale from -63,5dB (min) to 0dB (max).
+int A3HapticAmplitude = 5;  //Amplitude in [V] of alarm 1's haptic feedback. Input valid from 0-9V.
 
 // Audio alarm frequency and amplitude must be tweaked in socketaudio.py
 
 //*******************************************************************
+
+#include <Wire.h>
+int alarmNumber = 0; //variable to hold alarm number
+int alarmPeriod = 0; //variable holding alarm timeout period
+int ledPin1 = 3;  //pin number 1st led (RED) (PWM)
+int ledPin2 = 5;  //pin number 2nd led  (YELLOW) (PWM)
+int ledPin3 = 6;  //pin number 3rd led  (GREEN) (PWM)
+#define enA 10 //enable pin for haptic feedback
+#define in1 11 //in1 pin for haptic feedback
+#define in2 12 //in2 pin for haptic feedback
+int voltageValue = 0; //variable to hold haptic motor voltage value
+int pwmOutput = 0;  //variable to hold pwmOutput value to haptic motor
+
+int alarm1State = LOW;
+int alarm2State = LOW;
+int alarm3State = LOW;
+
+int newMessage = HIGH;
+int soundMessageState=LOW;
+
+unsigned long previousMillis = 0;
+unsigned long currentMillis = 0;
 
 void setup() {
   Wire.begin(1);                // join i2c bus with address #1
@@ -96,88 +100,133 @@ void setup() {
 }
 
 void loop() {
-  //  Writing float:
-  //  write_float(0xAA, 123.4141);
-
-  //  Writing integer:
-  //  write_int(0xAA, 5234);
-
-  Serial.println("alarmNumber at start of loop is: ");
-  Serial.println(alarmNumber);
-
   // Case 1-3 are for alarm scenario A (light, sound), case 4-6 are for alarm scenario B (light, sound, haptic)
 
   switch (alarmNumber) {
     case 1: //Alarm scenario 1A
-      ledAlarm(alarmNumber);
-      soundAlarm(alarmNumber);
-      delay(A1OnPeriod);
-      ledAlarm(0);
-      soundAlarm(0);
-      delay(A1OffPeriod);
+      if ((alarm1State == LOW && currentMillis - previousMillis >= A1OffPeriod) || newMessage == HIGH) {
+        previousMillis = currentMillis;
+        ledAlarm(alarmNumber);
+        soundAlarm(alarmNumber);
+        alarm1State = HIGH;
+        newMessage = LOW;
+      }
+      else if (alarm1State == HIGH && currentMillis - previousMillis >= A1OnPeriod) {
+        previousMillis = currentMillis;
+        ledAlarm(0);
+        soundAlarm(0);
+        alarm1State = LOW;
+        newMessage = LOW;
+      }
       break;
-    case 2: //Alarm scenario 2A
-      ledAlarm(alarmNumber);
-      soundAlarm(alarmNumber);
-      delay(A1OnPeriod);
-      ledAlarm(0);
-      soundAlarm(0);
-      delay(A1OffPeriod);
+    case 2: //Alarm scenario 1A
+      if ((alarm2State == LOW && currentMillis - previousMillis >= A1OffPeriod) || newMessage == HIGH) {
+        previousMillis = currentMillis;
+        ledAlarm(alarmNumber);
+        soundAlarm(alarmNumber);
+        alarm2State = HIGH;
+        newMessage = LOW;
+      }
+      else if (alarm2State == HIGH && currentMillis - previousMillis >= A1OnPeriod) {
+        previousMillis = currentMillis;
+        ledAlarm(0);
+        soundAlarm(0);
+        alarm2State = LOW;
+        newMessage = LOW;
+      }
       break;
-    case 3: //Alarm scenario 3A
-      ledAlarm(alarmNumber);
-      soundAlarm(alarmNumber);
-      delay(A3OnPeriod);
-      ledAlarm(0);
-      soundAlarm(0);
-      delay(A3OffPeriod);
+    case 3: //Alarm scenario 1A
+      if ((alarm3State == LOW && currentMillis - previousMillis >= A1OffPeriod) || newMessage == HIGH) {
+        previousMillis = currentMillis;
+        ledAlarm(alarmNumber);
+        soundAlarm(alarmNumber);
+        alarm3State = HIGH;
+        newMessage = LOW;
+      }
+      else if (alarm3State == HIGH && currentMillis - previousMillis >= A1OnPeriod) {
+        previousMillis = currentMillis;
+        ledAlarm(0);
+        soundAlarm(0);
+        alarm3State = LOW;
+        newMessage = LOW;
+      }
       break;
     case 4: //Alarm scenario 1B
-      ledAlarm(alarmNumber - 3);
-      soundAlarm(alarmNumber - 3);
-      hapticAlarm(alarmNumber - 3);
-      delay(A1OnPeriod);
-      ledAlarm(0);
-      soundAlarm(0);
-      hapticAlarm(0);
-      delay(A1OffPeriod);
+      if ((alarm1State == LOW && currentMillis - previousMillis >= A1OffPeriod) || newMessage == HIGH) {
+        previousMillis = currentMillis;
+        ledAlarm(alarmNumber - 3);
+        soundAlarm(alarmNumber - 3);
+        hapticAlarm(alarmNumber - 3);
+        alarm1State = HIGH;
+        newMessage = LOW;
+      }
+      else if (alarm1State == HIGH && currentMillis - previousMillis >= A1OnPeriod) {
+        previousMillis = currentMillis;
+        ledAlarm(0);
+        soundAlarm(0);
+        hapticAlarm(0);
+        alarm1State = LOW;
+        newMessage = LOW;
+      }
       break;
     case 5: //Alarm scenario 2B
-      ledAlarm(alarmNumber - 3);
-      soundAlarm(alarmNumber - 3);
-      hapticAlarm(alarmNumber - 3);
-      delay(A2OnPeriod);
-      ledAlarm(0);
-      soundAlarm(0);
-      hapticAlarm(0);
-      delay(A2OffPeriod);
+      if ((alarm2State == LOW && currentMillis - previousMillis >= A1OffPeriod) || newMessage == HIGH) {
+        previousMillis = currentMillis;
+        ledAlarm(alarmNumber-3);
+        soundAlarm(alarmNumber-3);
+        hapticAlarm(alarmNumber - 3);
+        alarm2State = HIGH;
+        newMessage = LOW;
+      }
+      else if (alarm2State == HIGH && currentMillis - previousMillis >= A1OnPeriod) {
+        previousMillis = currentMillis;
+        ledAlarm(0);
+        soundAlarm(0);
+        hapticAlarm(0);
+        alarm2State = LOW;
+        newMessage = LOW;
+      }
       break;
     case 6: //Alarm scenario 3B
-      ledAlarm(alarmNumber - 3);
-      soundAlarm(alarmNumber - 3);
-      hapticAlarm(alarmNumber - 3);
-      delay(A3OnPeriod);
-      ledAlarm(0);
-      soundAlarm(0);
-      hapticAlarm(0);
-      delay(A3OffPeriod);
+      if ((alarm3State == LOW && currentMillis - previousMillis >= A1OffPeriod) || newMessage == HIGH) {
+        previousMillis = currentMillis;
+        ledAlarm(alarmNumber-3);
+        soundAlarm(alarmNumber-3);
+        hapticAlarm(alarmNumber - 3);
+        alarm3State = HIGH;
+        newMessage = LOW;
+      }
+      else if (alarm3State == HIGH && currentMillis - previousMillis >= A1OnPeriod) {
+        previousMillis = currentMillis;
+        ledAlarm(0);
+        soundAlarm(0);
+        hapticAlarm(0);
+        alarm3State = LOW;
+        newMessage = LOW;
+      }
       break;
     case 0://Alarm scenario 0
       ledAlarm(0);
       hapticAlarm(0);
+
+      if(soundMessageState==LOW){
+        soundAlarm(0);
+        soundMessageState=HIGH;
+      }     
       break;
     default:
       Serial.println("Error in loop-switch input");
   }
-  delay(1000);
+  delay(100);
+  currentMillis = millis(); //update timer
 }
 
 // function that executes whenever data is received from master
 // this function is registered as an event, see setup()
 void receiveEvent(int howMany) {
   alarmNumber = Wire.read();    // receive byte as an integer
-  Serial.println("Newly received alarmNumber is: ");
-  Serial.print(alarmNumber);         // print the integer
+  newMessage = HIGH;
+  soundMessageState=LOW;
 }
 
 void ledAlarm(int ledAlarmNumber) {
@@ -207,34 +256,32 @@ void ledAlarm(int ledAlarmNumber) {
 }
 
 void soundAlarm(int soundAlarmNumber) {
+  write_int(17, soundAlarmNumber); //Sends packet requesting the correct sound alarm
   switch (soundAlarmNumber) {
     case 1: //Alarm scenario 1
-      write_int(16, 1);
+      write_float(19,A1Volume); //Sets alarm volume for alarm to A1Volume
       break;
     case 2: //Alarm scenario 2
-      write_int(16, 2);
+      write_float(19,A2Volume); //Sets alarm volume for alarm to A2Volume
       break;
     case 3: //Alarm scenario 3
-      write_int(16, 3);
+      write_float(19,A3Volume); //Sets alarm volume for alarm to A3Volume
       break;
-    case 0: //Disable alarms
-      write_int(16, 0);
       break;
 
   }
 }
 
-
 void hapticAlarm(int hapticAlarmNumber) {
   switch (hapticAlarmNumber) {
     case 1: //Alarm scenario 1
-      voltageValue = A1Amplitude; // Set voltage to A1Amplitude.
+      voltageValue = A1HapticAmplitude; // Set voltage to A1HapticAmplitude.
       break;
     case 2: //Alarm scenario 2
-      voltageValue = A2Amplitude; // Set voltage to A2Amplitude.
+      voltageValue = A2HapticAmplitude; // Set voltage to A2HapticAmplitude.
       break;
     case 3: //Alarm scenario 3
-      voltageValue = A3Amplitude; // Set voltage to A3Amplitude.
+      voltageValue = A3HapticAmplitude; // Set voltage to A3HapticAmplitude.
       break;
     case 0: //Alarm scenario 4
       voltageValue = 0; // Set voltage to 0V
