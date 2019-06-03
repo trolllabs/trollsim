c6f3104b3900ea1a6aa5a27dbc2beecf6e17dbbb
johnche <john20chen@gmail.com>
2019-06-03 20:09:24 +0200

    add colors to config, scatter+line feature and cleanup

--- data_visuals/scripts/config.py
+++ data_visuals/scripts/config.py
@@ -1,5 +1,9 @@
-PLOTWIDTH = 700
-PLOTHEIGHT = 400
+from bokeh.palettes import Category20_16, Spectral11
+
+LINE_COLOR = Category20_16
+SCATTER_COLOR = Spectral11
+PLOTWIDTH = 1000
+PLOTHEIGHT = 700
 sections = [
 		'SlideshowStart',
 		'SlideshowEnd',
--- data_visuals/scripts/plot_log.py
+++ data_visuals/scripts/plot_log.py
@@ -9,7 +9,8 @@ from pandas import DataFrame, to_datetime
 
 class MixedData:
 	def __init__(self, logdata: DataFrame):
-		self.dataset = None
+		self.line_dataset = None
+		self.scatter_dataset = None
 		self.logdata = logdata
 
 		# Partition markers
@@ -17,36 +18,41 @@ class MixedData:
 		self.logdata = self.logdata[~self.logdata.Name.isin(conf.sections)]
 		self.logdata.Timestamp = to_datetime(self.logdata.Timestamp, unit='ms')
 
+		# Scatter data
+		self.scatter_data = self.logdata
+
 		# Multi line data prep
-		self.logdata = self.logdata.groupby('Channel').aggregate({'Timestamp': list, 'Value': list})
+		self.line_data = self.logdata.groupby('Channel').aggregate({'Timestamp': list, 'Value': list})
 
-
-		self.id_list = list(self.logdata.index)
+		self.id_list = list(self.line_data.index)
 		self.id_list.sort()
 		self.line_channel_checkbox = CheckboxGroup(labels=self.id_list)
 		self.line_channel_checkbox.on_change('active', self.lineplot_handler)
 		self.scatter_channel_checkbox = CheckboxGroup(labels=self.id_list)
 		self.scatter_channel_checkbox.on_change('active', self.scatterplot_handler)
 
-	def generate_dataset(self, log_data, active_channels):
-		visible_data = DataFrame(columns=['Timestamp', 'Value', 'channel', 'color'])
+	def generate_scatter_dataset(self, log_data, active_channels):
+		visible_data = DataFrame(columns=['Timestamp', 'Value', 'Channel', 'color'])
 
 		for i, channel in enumerate(active_channels):
-			#visible_channel = log_data[log_data['Channel'] == channel]
-			#visible_channel.color = Category20_16[i]
-			#visible_data = visible_data.append(visible_channel, sort=True)
-
-			#print('new visible channel', channel)
-			visible_channel = log_data.loc[channel]
-			visible_channel['color'] = Category20_16[i]
-			visible_channel['channel'] = channel
+			visible_channel = log_data[log_data['Channel'] == channel]
+			visible_channel['color'] = conf.SCATTER_COLOR[i]
 			visible_data = visible_data.append(visible_channel, sort=True)
-
-		print(visible_data)
 
 		return ColumnDataSource(visible_data)
 
-	def generate_figure(self, data: ColumnDataSource):
+	def generate_line_dataset(self, log_data, active_channels):
+		visible_data = DataFrame(columns=['Timestamp', 'Value', 'Channel', 'color'])
+
+		for i, channel in enumerate(active_channels):
+			visible_channel = log_data.loc[channel]
+			visible_channel['color'] = conf.LINE_COLOR[i]
+			visible_channel['Channel'] = channel
+			visible_data = visible_data.append(visible_channel, sort=True)
+
+		return ColumnDataSource(visible_data)
+
+	def generate_figure(self, line_data: ColumnDataSource, scatter_data: ColumnDataSource):
 		fig = figure(
 				title='Log',
 				plot_width=conf.PLOTWIDTH,
@@ -55,35 +61,28 @@ class MixedData:
 				y_axis_label='Value'
 				)
 
-		#print(self.id_list)
-		#fig.scatter('Timestamp', 'Value', source=data, legend='channel', marker='circle', color='color')
-		fig.multi_line('Timestamp', 'Value', source=data, line_width=2, color='color', legend='channel')
-
-		fig.scatter('Timestamp', 'Value', source=data, legend='Name', marker='circle', color='color')
-
-		#fig.line('Timestamp', 'Value', source=data, line_width=2)
-		#for view in self.views:
-		#	fig.line('Timestamp', 'Value', source=data, legend='Name', view=view)
-		#fig.line('Timestamp', 'Value', source=data, legend='Name')
-
+		fig.multi_line('Timestamp', 'Value', source=line_data, line_width=2, color='color', legend='Channel')
+		fig.scatter('Timestamp', 'Value', source=scatter_data, legend='Channel', marker='circle', color='color')
 		return fig
-
-	def update(self, active_channels):
-		new_dataset = self.generate_dataset(self.logdata, active_channels)
-		self.dataset.data.update(new_dataset.data)
 
 	def scatterplot_handler(self, attr, old, new):
 		active_channels = [self.scatter_channel_checkbox.labels[i] for i in new]
-		self.update(active_channels)
+		new_dataset = self.generate_scatter_dataset(self.scatter_data, active_channels)
+		self.scatter_dataset.data.update(new_dataset.data)
 
 	def lineplot_handler(self, attr, old, new):
 		active_channels = [self.line_channel_checkbox.labels[i] for i in new]
-		self.update(active_channels)
+		new_dataset = self.generate_line_dataset(self.line_data, active_channels)
+		self.line_dataset.data.update(new_dataset.data)
 
 	def tab(self, name):
-		initial_channels = [self.line_channel_checkbox.labels[i] for i in self.line_channel_checkbox.active]
-		self.dataset = self.generate_dataset(self.logdata, initial_channels)
-		fig = self.generate_figure(self.dataset)
+		initial_line_channels = [self.line_channel_checkbox.labels[i] for i in self.line_channel_checkbox.active]
+		self.line_dataset = self.generate_line_dataset(self.line_data, initial_line_channels)
+
+		initial_scatter_channels = [self.scatter_channel_checkbox.labels[i] for i in self.scatter_channel_checkbox.active]
+		self.scatter_dataset = self.generate_scatter_dataset(self.scatter_data, initial_scatter_channels)
+
+		fig = self.generate_figure(self.line_dataset, self.scatter_dataset)
 
 		# For sectioning the figure
 		for span_location in self.span_locations.itertuples():
